f31e8d49b52dab4ea589d1162bcacfef721c305d
Josiah Morris <josiahbmorris@hotmail.com>
2024-08-13 04:20:15 +0900

    Added basic habit view page

---
+++ frontend/src/routes/[habitID]/+page.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+  import { habits } from "../../stores/habits"
+  export let data
+  $: id = data.id
+  $: habit = $habits.get(id)
+</script>
+
+<h1>{habit.name}</h1>
+<ul>
+  {#each habit.instances as instance}
+    <li>{instance.date}</li>
+  {/each}
+</ul>
