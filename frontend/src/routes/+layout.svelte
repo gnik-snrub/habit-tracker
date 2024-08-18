ce6152e33d8958772b8f428f9051038273e8376a
Josiah Morris <josiahbmorris@hotmail.com>
2024-08-19 02:32:12 +0900

    Styled main areas in root +layout

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -8,22 +8,32 @@
     <h1>Habit Tracker</h1>
   </a>
 </header>
-<Sidebar/>
-<slot/>
+<main>
+  <Sidebar/>
+  <slot/>
+</main>
 
 
 <style>
-  :global(body) {
+  :global(body, html) {
     margin: 0;
+    height: 100%;
   }
 
   header {
     margin: 0;
     background-color: rgb(238, 238, 238);
+    height: 10vh;
   }
   h1 {
     padding: 10px;
     margin: 0;
     margin-left: 10px;
+  }
+
+  main {
+    display: flex;
+    flex-flow: row;
+    height: 90vh;
   }
 </style>
