4d819f192ff602e6010f287d1e7db23a3e9c1948
Josiah Morris <josiahbmorris@hotmail.com>
2024-08-23 04:01:59 +0900

    Fixed backends app.js file

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,23 @@
+const apiRouter = require('./routes/api')
+
+const mongoose = require('mongoose')
 const express = require('express')
+require('dotenv').config()
+
+mongoose.set('strictQuery', false)
+const mongoDB = process.env.MONGO_CT_URL
+main().catch(err => console.log(err))
+async function main() { await mongoose.connect(mongoDB) }
 
 const app = express()
-const PORT = 3000
 
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
+const bodyParser = require('body-parser')
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+
+const cors = require('cors')
+app.use(cors())
+
+app.use('/', apiRouter)
+
+app.listen(process.env.PORT, () => console.log(`Server running on port ${process.env.PORT}`))
