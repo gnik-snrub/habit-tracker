1676819c60f063830fd707d44df1b27a3e45efdb
Josiah Morris <josiahbmorris@hotmail.com>
2024-08-23 03:59:53 +0900

    Added getHabits controller

---
+++ backend/controllers/apiController.js
@@ -0,0 +1,12 @@
+const Habit = require('../models/Habit')
+const User = require('../models/User')
+
+exports.getHabits = async(req, res) => {
+  const user = await User.findById(req.params.userID)
+  if (!user) {
+    return res.json({ error: 'User not found' })
+  }
+  const habits = await Habit.find({ user: user._id }, 'name instances')
+  res.json(habits)
+}
+
