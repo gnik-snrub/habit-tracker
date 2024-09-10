fb75095c621458c2cdc9b9eb23b0fdc9dc1a0acd
Josiah Morris <josiahbmorris@hotmail.com>
2024-09-11 03:34:36 +0900

    Added generic Button component

---
+++ frontend/src/lib/Button.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+  export let data: {label: string, func?: () => void}
+  const { label, func } = data
+  
+</script>
+
+<button on:click={func}>{label}</button>
+
+<style>
+  button {
+    border: 1px solid var(--colorOne);
+    color: var(--colorOne);
+    background-color: var(--colorTwo);
+    height: fit-content;
+    padding: 0.3em 1em;
+    border-radius: 1em;
+    transition: 0.3s;
+    margin: 0 1em;
+  }
+  button:hover, button:focus {
+    border: 1px solid var(--colorTwo);
+    color: var(--colorTwo);
+    background-color: var(--colorOne);
+  }
+</style>
