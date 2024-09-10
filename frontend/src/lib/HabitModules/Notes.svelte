dd9fa8797f1eadd78042948946e726418bfedf10
Josiah Morris <josiahbmorris@hotmail.com>
2024-09-11 03:38:37 +0900

    Restyled Notes module

--- frontend/src/lib/HabitModules/Notes.svelte
+++ frontend/src/lib/HabitModules/Notes.svelte
@@ -3,6 +3,7 @@
   import { habits } from "../../stores/habits";
 
   import Button from "$lib/Button.svelte";
+
   export let habit: Habit
   export let updateHabitStore: () => Promise<void>
 
@@ -17,6 +18,7 @@
     body.append('name', updatedHabit.name)
     body.append('instances', updatedHabit.instances.toString())
     body.append('notes', updatedHabit.notes)
+    body.append('layout', updatedHabit.layout)
 
     const updateResponse = await fetch(`${import.meta.env.VITE_API_DOMAIN}/habits`, {
       method: 'PUT',
@@ -31,15 +33,24 @@
 
 </script>
 
-<form on:submit|preventDefault={saveNotes}>
-  <textarea name="notes" id="" cols="30" rows="10" value={habit.notes}></textarea>
-</form>
+<section>
+  <h3>Notes</h3>
+  <form on:submit|preventDefault={saveNotes}>
+    <textarea name="notes" id="" cols="30" rows="10" value={habit.notes ? habit.notes : ''}></textarea>
     <Button
       --colorOne="var(--dark-text-color)" --colorTwo="var(--dark-bg-color)"
       data={{ label: 'Save' }}
     />
+  </form>
+</section>
 
 <style>
+  section {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    width: 100%;
+  }
   form {
     width: 100%;
     display: flex;
@@ -50,10 +61,12 @@
   textarea {
     width: 80%;
     resize: none;
-    border: none;
+    border: 1px solid var(--accent-color);
     outline: none;
     padding: 1em;
     margin-bottom: 1em;
     border-radius: 5px;
+    background-color: var(--dark-bg-shadow-color);
+    color: inherit;
   }
 </style>
