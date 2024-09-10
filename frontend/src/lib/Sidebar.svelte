5c5902159609fbe9e3cbee8d6a696d1c1292a78e
Josiah Morris <josiahbmorris@hotmail.com>
2024-09-11 03:38:45 +0900

    Restyled Sidebar

--- frontend/src/lib/Sidebar.svelte
+++ frontend/src/lib/Sidebar.svelte
@@ -73,8 +73,9 @@
     display: flex;
     flex-direction: column;
     align-items: center;
-    border-right: 1px solid black;
+    border-right: 1px solid var(--accent-color);
     height: 100%;
+    width: 100%;
   }
   ul {
     position: relative;
@@ -87,18 +88,46 @@
     overflow: auto;
     overflow-x: hidden;
     & li:nth-child(even) {
-      background-color: lightgray;
+      background-color: var(--dark-bg-shadow-color);
+    }
+  }
+  form {
+    background-color: var(--dark-bg-shadow-color);
+    width: 100%;
+    display: flex;
+    border-bottom: 1px solid var(--accent-color);
+    & > input {
+      padding: 0.1em 0.5em;
+      border: 1px solid var(--accent-color);
+      width: 100%;
+      background-color: var(--dark-bg-shadow-color);
+      color: var(--dark-text-color);
+      border: none;
+    }
+    & > button {
+      width: auto;
+      border: none;
+      color: var(--dark-text-color);
+      background-color: inherit;
     }
   }
   li {
     list-style: none;
     display: grid;
-    grid-template-rows: 1fr 1fr;
-    padding-left: 1em;
+    align-items: center;
+    grid-template-rows: 1fr 0.7fr;
+    padding-left: 2em;
+    padding-right: 2em;
     width: 100%;
     & a {
       text-decoration: none;
       color: inherit;
+      overflow: hidden;
+      white-space: nowrap;
+    }
+    & > * {
+      padding: 0 0.7em;
+      margin: 0;
     }
   }
 </style>
