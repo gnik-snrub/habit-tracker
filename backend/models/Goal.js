8a3e8fc05b21a3675027e213b29d7160a8dbeea0
Josiah Morris <josiahbmorris@hotmail.com>
2024-09-17 05:22:47 +0900

    Created Goal Schema

---
+++ backend/models/Goal.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose')
+
+const Schema = mongoose.Schema
+
+const GoalSchema = new Schema({
+  name: { type: String, required: true },
+  habit: { type: Schema.Types.ObjectId, ref: 'Habit', required: true },
+  user: { type: Schema.Types.ObjectId, ref: 'User', required: true },
+  creationDate: { type: Date, required: true },
+  startDate: { type: Date, required: false },
+  endDate: { type: Date, required: false },
+  goalTarget: { type: Number, required: false },
+  goalFrequency: { type: {
+    timeframe: { type: String, enum: ['Daily', 'Weekly', 'Monthly']},
+    amount: { type: number}},
+    required: false },
+  goalCompleted: { type: Boolean, required: false },
+})
+
+module.exports = mongoose.model('Goal', GoalSchema)
