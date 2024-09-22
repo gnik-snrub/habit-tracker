5b9428b5b096770eab4bd736e15b760e4d51d3f7
Josiah Morris <josiahbmorris@hotmail.com>
2024-09-23 05:08:03 +0900

    Added backend routes for goal management

--- backend/routes/api.js
+++ backend/routes/api.js
@@ -7,4 +7,9 @@ router.post('/habits', apiController.createHabit)
 router.delete('/habits', apiController.deleteHabit)
 router.put('/habits', apiController.updateHabit)
 
+router.get('/goals/:habitID', apiController.getGoals)
+router.post('/goals', apiController.createGoal)
+router.delete('/goals', apiController.deleteGoal)
+router.put('/goals', apiController.updateGoal
+
 module.exports = router
