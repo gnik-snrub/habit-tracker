b898b8e2c0aeb7ce592abed296ebe5e063775352
Josiah Morris <josiahbmorris@hotmail.com>
2024-09-24 23:19:04 +0900

    Corrected updateGoal api call error

--- backend/routes/api.js
+++ backend/routes/api.js
@@ -10,6 +10,6 @@ router.put('/habits', apiController.updateHabit)
 router.get('/goals/:habitID', apiController.getGoals)
 router.post('/goals', apiController.createGoal)
 router.delete('/goals', apiController.deleteGoal)
-router.put('/goals', apiController.updateGoal
+router.put('/goals', apiController.updateGoal)
 
 module.exports = router
