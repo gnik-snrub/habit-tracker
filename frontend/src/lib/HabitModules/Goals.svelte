5450b380ee4c020e7fa295756e416969eb2d723d
Josiah Morris <josiahbmorris@hotmail.com>
2024-10-01 04:28:34 +0900

    Added simple Goals module

---
+++ frontend/src/lib/HabitModules/Goals.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+  export let habit: Habit
+  export let updateHabitStore: () => Promise<void>
+  updateHabitStore = (): Promise<void> => { return }
+
+  import { goals as goalsData } from "../../stores/goals"
+
+  let goals = $goalsData.get(habit._id)
+</script>
+
+
+<div>
+  <h3>Goals</h3>
+  <ul>
+    {#each goals as goal}
+      <li>{goal.name}</li>
+    {/each}
+  </ul>
+</div>
+
+<style>
+  li {
+    list-style: number;
+  }
+</style>
