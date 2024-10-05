b112ffe64846709013ae8f5aa7f3cf0cc5244569
Josiah Morris <josiahbmorris@hotmail.com>
2024-10-06 02:07:11 +0900

    Changed from using chartjs-chart-matrix to using cal-heatmap for CalendarHeatmap

--- frontend/src/lib/HabitModules/CalendarHeatmap.svelte
+++ frontend/src/lib/HabitModules/CalendarHeatmap.svelte
@@ -1,20 +1,14 @@
 <script lang="ts">
-export let habit: Habit
-import { afterUpdate } from 'svelte';
-import { Chart } from 'chart.js/auto'
-import { MatrixController, MatrixElement } from 'chartjs-chart-matrix'
+  import CalHeatmap from 'cal-heatmap'
+  import 'cal-heatmap/cal-heatmap.css'
+  import Tooltip from 'cal-heatmap/plugins/Tooltip'
+  import {afterUpdate} from 'svelte';
 
-Chart.register(MatrixController, MatrixElement)
+  export let habit: Habit
+  export let updateHabitStore: () => Promise<void>
+  updateHabitStore = (): Promise<void> => { return }
 
-let heatmapChart: Chart
-let canvasElement: HTMLCanvasElement
-
-function buildChart() {
-  canvasElement = document.getElementById("heatmapCalendar") as HTMLCanvasElement
-  if (heatmapChart) {
-    heatmapChart.destroy()
-  }
-  const instanceData = habit.instances.reduce((acc, curr) => {
+  $: instanceData = habit.instances.reduce((acc, curr) => {
     const currDate = curr.toString().slice(0, 10)
     const index = acc.findIndex(obj => obj.date === currDate)
     if (index === -1) {
@@ -24,80 +18,78 @@ function buildChart() {
     }
     return acc
   }, [])
-    /*{
-      type: 'matrix',
-      data: {
-        labels: instanceData.map(obj => obj.date),
-        datasets: [
-          {
-            label: 'Instances',
-            data: instanceData.map(obj => obj.count),
-          }
-        ]
-      }
-    }
-    */
-    const h = 5
-    const w = 4
-    const config= {
-      type: 'matrix',
-      data: {
-        datasets: [{
-          label: 'Basic Matrix',
-          data: [{x: 1, y: 1}, {x: 2, y: 1}, {x: 1, y: 2}, {x: 2, y: 2}, {x: 1, y: 3}, {x: 2, y: 3}, {x: 1, y: 4}, {x: 2, y: 4}, {x: 3, y: 1}, {x: 3, y: 2}, {x: 3, y: 3}, {x: 3, y: 4}, {x: 4, y: 1}, {x: 4, y: 2}, {x: 4, y: 3}, {x: 4, y: 4}],
-          borderWidth: 5,
-          borderColor: 'rgba(0, 0, 0, 0.5)',
-          backgroundColor: 'rgba(200, 200, 0, 0.3)',
-          width: ({chart}) => (chart.chartArea || {}).width / w - 1,
-          height: ({chart}) => (chart.chartArea || {}).height / h - 1,
-        }],
+
+  const cal: CalHeatmap = new CalHeatmap()
+
+  function generateHeatmap() {
+    cal.paint({
+      itemSelector: '#cal-heatmap',
+      domain: {
+        type: 'month',
+        label: {
+          position: 'top',
+        }
       },
-      options: {
-        scales: {
-          x: {
-            display: true,
-            min: 0.5,
-            max: w + 0.5,
-            offset: false
-          },
-          y: {
-            display: true,
-            min: 0,
-            max: h,
-            offset: false
-          }
+      subDomain: {
+        type: 'day',
+      },
+      date: {
+        start: new Date(new Date().setMonth(new Date().getMonth() - 11)),
+        highlight: [new Date()],
+      },
+      data: {
+        source: instanceData,
+        x: (data: { date: string, count: number }) => data.date,
+        y: (data: { date: string, count: number }) => data.count,
+        groupY: 'average',
+        defaultValue: 0,
+      },
+      scale: {
+        color: {
+          range: ['rgb(39, 154, 241)', 'rgb(252, 239, 249)'],
+          type: 'linear',
+          domain: [0, 7],
         }
       }
-    }
-  heatmapChart = new Chart(canvasElement, config)
-}
+    },
+      [
+        [Tooltip,
+          {
+            text: (date, value, dayjsDate) => `${value} engagements with ${habit.name} on ${dayjsDate.format('DD/MM/YYYY')}`,
+          }
+        ],
+      ]
+    )
+  }
 
-afterUpdate(buildChart)
+  afterUpdate(generateHeatmap)
 </script>
 
 <div>
-  <h3>Calendar</h3>
-  <canvas id="heatmapCalendar" height="20" width="100"></canvas>
+  <h3>
+    Calendar
+  </h3>
+  <div id="calendarWrapper">
+    <div id="cal-heatmap"></div>
+  </div>
 </div>
 
 <style>
   h3 {
     rotate: -90deg;
   }
-  div {
+  div:not(#calendarWrapper, #cal-heatmap) {
     width: 100%;
-    height: 30vh;
     margin: 1.5em 0;
     padding: 1em 0;
     outline: 1px solid var(--accent-color);
     background-color: var(--dark-bg-shadow-color);
     display: flex;
     align-items: center;
+    overflow-x: auto;
   }
-  canvas {
-    width: 100%;
-    height: 100%;
-    overflow: hidden;
-    justify-self: center;
+  #calendarWrapper {
+    margin-right: 1em;
+    overflow-x: auto;
   }
 </style>
