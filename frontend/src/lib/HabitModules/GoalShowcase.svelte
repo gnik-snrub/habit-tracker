0d977363e0674bf93520c870f905114ecf3cd011
Josiah Morris <josiahbmorris@hotmail.com>
2024-10-10 05:15:06 +0900

    GoalShowcase now retrieves correct goal data

---
+++ frontend/src/lib/HabitModules/GoalShowcase.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+    import {onMount} from "svelte";
+
+  import { goals as goalsData } from "../../stores/goals"
+  export let props: {habit: Habit, goal: string}
+  $: habit = props.habit
+  $: goal = $goalsData.get(habit._id).find((goal) => goal._id === props.goal)
+
+  onMount(() => {
+    console.log(habit, goal)
+  })
+</script>
+
+<section id="goal">
+  <h4>{goal.name}</h4>
+  <p>Created on:</p>
+  <p>{new Date(goal.creationDate).toLocaleString()}</p>
+</section>
