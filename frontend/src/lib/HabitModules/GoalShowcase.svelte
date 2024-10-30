13925af44b70df34bfaf9bfffa29abebb497230d
Josiah Morris <josiahbmorris@hotmail.com>
2024-10-31 05:17:14 +0900

    Added form to complete goal, if target has been met

--- frontend/src/lib/HabitModules/GoalShowcase.svelte
+++ frontend/src/lib/HabitModules/GoalShowcase.svelte
@@ -1,7 +1,11 @@
 <script lang="ts">
   import {afterUpdate} from "svelte";
   import { goals as goalsData } from "../../stores/goals"
+  import {userData} from "../../stores/userData";
+  import Button from "../Button.svelte";
+
   export let props: {habit: Habit, goal: string}
+
   $: habit = props.habit
   $: goal = $goalsData.get(habit._id).find((goal) => goal._id === props.goal)
   $: checkInsInRange = findCheckInsInRange(habit)
@@ -14,12 +18,57 @@
         checkIns++
       }
     }
+    if (checkIns > goal.goalTarget) {
+      checkIns = goal.goalTarget
+    }
     return checkIns
   }
 
   afterUpdate(() => {
     checkInsInRange = findCheckInsInRange(habit)
   })
+
+  async function completeGoal() {
+    const data = new URLSearchParams()
+    data.append('goalID', goal._id)
+    if (goal.startDate) {
+      data.append('startDate', goal.startDate)
+    }
+    if (goal.endDate) {
+      data.append('endDate', goal.endDate)
+    }
+    if (goal.goalTarget) {
+      data.append('goalTarget', goal.goalTarget)
+    }
+    if (goal.goalFrequency) {
+      data.append('timeframe', goal.goalFrequency.timeframe)
+      data.append('amount', goal.goalFrequency.amount)
+    }
+    data.append('completed', true)
+    data.append('goalCompleted', true)
+    const completeGoalResponse = await fetch(`${import.meta.env.VITE_API_DOMAIN}/goals`, {
+      method: 'PUT',
+      body: data
+    })
+    const { completedGoalId } = await completeGoalResponse.json()
+    console.log('Completed goal', completedGoalId)
+
+    const goalRetriveResponse = await fetch(`${import.meta.env.VITE_API_DOMAIN}/goals/${$userData}`)
+    const fetchedGoals = await goalRetriveResponse.json()
+
+    const tempGoals: Map<string, Goal[]> = new Map()
+    fetchedGoals.forEach((goal: Goal) => {
+      if (tempGoals.has(goal.habit)) {
+        tempGoals.get(goal.habit).push(goal)
+      } else {
+        tempGoals.set(goal.habit, [goal])
+      }
+    })
+    goalsData.set(tempGoals)
+    console.log('Refreshed goals')
+  }
+
+  let confirmCompleteToggle = false
 </script>
 
 <h3>{goal.name}</h3>
@@ -58,6 +107,18 @@
       <p>{goal.goalCompleted ? "Complete!" : "In Progress"}</p>
     </div>
   {/if}
+  {#if checkInsInRange === goal.goalTarget && !goal.goalCompleted}
+    <div id="completeGoal">
+      {#if !confirmCompleteToggle}
+        <Button --colorOne="var(--dark-text-color)" --colorTwo="var(--dark-bg-shadow-color)" data={{ func: () => {confirmCompleteToggle = true}}}>Complete Goal</Button>
+      {:else}
+        <form on:submit|preventDefault={completeGoal}>
+          <Button --colorOne="var(--dark-text-color)" --colorTwo="var(--dark-bg-shadow-color)" data={{ func: () => {}}}>Confirm</Button>
+        </form>
+        <Button --colorOne="var(--dark-text-color)" --colorTwo="var(--dark-bg-shadow-color)" data={{ func: () => {confirmCompleteToggle = false}}}>Cancel</Button>
+      {/if}
+    </div>
+  {/if}
 </section>
 
 <style>
@@ -78,6 +139,13 @@
     white-space: nowrap;
     background-color: hsl(from var(--dark-bg-shadow-color) h s calc(l*1.05) );
     border-radius: 15px;
+  }
+  #completeGoal {
+    width: 100%;
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    align-items: center;
   }
   p {
     margin: 0 1em;
