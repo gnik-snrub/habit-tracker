13e2640786f6f585be88a76dc99c8ccbb3932e91
Josiah Morris <josiahbmorris@hotmail.com>
2024-11-03 03:05:20 +0900

    Added verifyToken middleware to api routes

--- backend/routes/api.js
+++ backend/routes/api.js
@@ -1,16 +1,29 @@
+const jwt = require('jsonwebtoken')
+
 const express = require('express')
 const router = express.Router()
+
 const apiController = require('../controllers/apiController')
 
-router.get('/habits/:userID', apiController.getHabits)
-router.post('/habits', apiController.createHabit)
-router.delete('/habits', apiController.deleteHabit)
-router.put('/habits', apiController.updateHabit)
+function verifyToken(req, res, next) {
+  const token = req.headers.authorization.split(' ')[1]
+  const payload = jwt.verify(token, process.env.JWT_SECRET)
+  if (!payload) {
+    return res.status(401).send('Unauthorized request')
+  }
+  req.userId = payload.subject
+  next()
+}
 
-router.get('/goals/:userID', apiController.getGoals)
-router.post('/goals', apiController.createGoal)
-router.delete('/goals', apiController.deleteGoal)
-router.put('/goals', apiController.updateGoal)
+router.get('/habits/:userID', verifyToken, apiController.getHabits)
+router.post('/habits', verifyToken, apiController.createHabit)
+router.delete('/habits', verifyToken, apiController.deleteHabit)
+router.put('/habits', verifyToken, apiController.updateHabit)
+
+router.get('/goals/:userID', verifyToken, apiController.getGoals)
+router.post('/goals', verifyToken, apiController.createGoal)
+router.delete('/goals', verifyToken, apiController.deleteGoal)
+router.put('/goals', verifyToken, apiController.updateGoal)
 
 router.post('/register', apiController.register)
 router.post('/login', apiController.login)
