b040b14a7549f6d6b069bacc9c9c51723ab10485
Josiah Morris <josiahbmorris@hotmail.com>
2024-11-04 04:34:33 +0900

    Setup compression, helmet, and rate limiter in backend

--- backend/app.js
+++ backend/app.js
@@ -9,7 +9,19 @@ const mongoDB = process.env.MONGO_CT_URL
 main().catch(err => console.log(err))
 async function main() { await mongoose.connect(mongoDB) }
 
+const compression = require('compression')
+const helmet = require('helmet')
+const RateLimit = require('express-rate-limit')
+
 const app = express()
+
+app.use(compression())
+app.use(helmet())
+const limiter = RateLimit({
+  windowMs: 1 * 60 * 1000, // 1 minute
+  max: 100,
+})
+app.use(limiter)
 
 const bodyParser = require('body-parser')
 app.use(bodyParser.json())
